6df43a0e90a94c38dfd1f627175d372d46c1ff5e
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-27 18:13:40 -0500

    creating routes

---
+++ src/components/ContainerJob.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/styles/components/ContainerJob.scss';
+
+const ContainerJob = () => (
+  <div className="container-job">
+    <div className="container-job-details">
+      <h2 className="container-job-details-title">Full Stack Engineer</h2>
+      <p>A full time team member or employee</p>
+      <p>1 months ago</p>
+    </div>
+  </div>
+);
+
+export default ContainerJob;
--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
+import ContainerJob from '../components/ContainerJob';
+
 import '../assets/styles/App.scss';
 
 const App = () => (
   <div className="App">
     <Header />
     <Search />
+    <ContainerJob />
   </div>
 );
 
