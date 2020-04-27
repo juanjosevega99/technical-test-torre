32c42133054118642766d7717f8ddaa2d8274691
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-27 16:43:19 -0500

    searcher

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/Search.scss';
+
+const Search = () => (
+  <section className="main">
+    <h2 className="main__title">Skills for job?</h2>
+    <input type="text" className="input" placeholder="Search..." />
+  </section>
+);
+
+export default Search;
