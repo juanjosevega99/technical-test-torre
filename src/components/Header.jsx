192915eff33a3eafaa882f5fd461f037563d34f9
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-27 16:37:22 -0500

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import '../assets/styles/components/Header.scss';
 
-const Header = () => <div>Header</div>;
+import logo from '../assets/static/torre.png';
+import userIcon from '../assets/static/user-icon.png';
+
+const Header = () => (
+  <header className="header">
+    <img className="header__img" src={logo} alt="torre" />
+    <div className="header__menu">
+      <div className="header__menu--profile">
+        <img src={userIcon} alt="" />
+        <p>Profile</p>
+      </div>
+      <ul>
+        <li>
+          <a href="/">ACCOUNT</a>
+        </li>
+        <li>
+          <a href="/">SIGN OUT</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+);
 
 export default Header;
