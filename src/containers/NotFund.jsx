0824ca266b6ce84937495fdf5d10afd1928be125
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-27 23:12:31 -0500

    container not found

---
+++ src/containers/NotFund.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFund = () => (
+  <>
+    <h1>Not found</h1>
+  </>
+);
+
+export default NotFund;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
-import Home from '../container/Home';
+import Home from '../containers/Home';
+import NotFound from '../containers/NotFund';
 
 const App = () => (
   <BrowserRouter>
     <Route exact path="/" component={Home} />
+    <Route component={NotFound} />
   </BrowserRouter>
 )
 
