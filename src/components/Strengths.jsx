48853dc4a41cc99fbf382e75e16e131c00f2a2f3
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-27 23:57:00 -0500

    corrections in css

---
+++ src/components/Strengths.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/components/Strengths.scss';
+
+import logo from '../assets/static/company.png';
+
+const dataUser = 'https://torre.bio/api/bios/juanjosevegaq';
+const dataCompany = 'https://torre.co/api/opportunities/NwqxZlW3';
+
+const ContainerJob = () => (
+  <div className="">{/* implement info and Strengths */}</div>
+);
+
+export default ContainerJob;
