30a516d3aa4a6f7ef1d6c304cee9a1f4a6f2d4bf
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-28 07:28:57 -0500

    setting the server to status

--- server/server.js
+++ server/server.js
@@ -77,3 +77,53 @@ const renderApp = (req, res) => {
   );
   res.send(setResponse(html, preloadedState, req.hashManifest));
 };
+
+app.post("/auth/sign-in", async function (req, res, next) {
+  passport.authenticate("basic", function (error, data) {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+
+      req.login(data, { session: false }, async function (error) {
+        if (error) {
+          next(error);
+        }
+
+        const { token, ...user } = data;
+
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post("/auth/sign-up", async function (req, res, next) {
+  const { body: user } = req;
+
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: "post",
+      data: user,
+    });
+
+    res.status(201).json({ message: "user created" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("*", renderApp);
+
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  else console.log(`${ENV} server running on Port ${PORT}`);
+});
